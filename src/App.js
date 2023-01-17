3dd15cfe9f0e8b9571373d6a21f82ec6f98a1615
ntelio96 <mantas3456@gmail.com>
2023-01-17 16:49:55 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 
 function App() {
@@ -22,38 +23,14 @@ function App() {
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
-    },
+    }
   ];
 
   return (
-    <>
-      {/* <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      /> */}
-
-      { expenses.map((expense, index) => <ExpenseItem key={index} title={expense.title}
-        amount={expense.amount}
-        date={expense.date}/>)
-      }
-        
-    </>
+    <div className="expenses">
+      <Expenses  
+        expenses = {expenses}/>
+    </div>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Card(props) {
+    return (
+        <div className='card'>
+            
+        </div>
+    );
+}
+
+export default Card;
+
+//rsf
